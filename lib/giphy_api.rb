cfda24a73bef6a326137a29914656c73d03679d7
Matthew Jording <mjording@gmail.com>
2015-08-12 21:00:31 -0400

    simple faraday implementation

--- lib/giphy_api.rb
+++ lib/giphy_api.rb
@@ -1,5 +1,15 @@
 require "giphy_api/version"
+require "faraday_middleware"
 
 module GiphyApi
-  # Your code goes here...
+  class Client
+
+    def initialize
+      @connection = Faraday.new('http://api.giphy.com')
+    end
+
+    def trending
+      @connection.get '/v1/gifs/trending', { :api_key => 'dc6zaTOxFJmzC' }
+    end
+  end
 end
