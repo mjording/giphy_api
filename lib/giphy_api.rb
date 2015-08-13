ada0c3aba076669f7744ff45ececcfc66a3afbbf
yen168 <yenan.chen@baruchmail.cuny.edu>
2015-08-13 16:50:18 -0400

    Search function

--- lib/giphy_api.rb
+++ lib/giphy_api.rb
@@ -8,8 +8,19 @@ module GiphyApi
       @connection = Faraday.new('http://api.giphy.com')
     end
 
+    def search(query)
+      #[q,limit,offset,rating,fmt]
+      query_string = query.inject(""){|a,item| a << "&#{item[0]}=#{item[1]}"}
+      @connection.get "/v1/gifs/search?#{query_string}", { :api_key => 'dc6zaTOxFJmzC' }
+    end
+
     def trending
       @connection.get '/v1/gifs/trending', { :api_key => 'dc6zaTOxFJmzC' }
     end
+
+    def random
+      @connection.get '/v1/gifs/random', { :api_key => 'dc6zaTOxFJmzC' }
+    end
+
   end
 end
