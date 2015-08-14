2abadc4051dc6e511e43da07561eaccd413d7d02
yen168 <yenan.chen@baruchmail.cuny.edu>
2015-08-14 10:19:52 -0400

    refactor

--- lib/giphy_api.rb
+++ lib/giphy_api.rb
@@ -6,20 +6,36 @@ module GiphyApi
 
     def initialize
       @connection = Faraday.new('http://api.giphy.com')
+      @api_key ||= { :api_key => 'dc6zaTOxFJmzC' }
     end
 
     def search(query)
       #[q,limit,offset,rating,fmt]
-      query_string = query.inject(""){|a,item| a << "&#{item[0]}=#{item[1]}"}
-      @connection.get "/v1/gifs/search?#{query_string}", { :api_key => 'dc6zaTOxFJmzC' }
+      @connection.get "/v1/gifs/search?#{get_paras(query)}", @api_key
     end
 
     def trending
-      @connection.get '/v1/gifs/trending', { :api_key => 'dc6zaTOxFJmzC' }
+      #[limit,rating,fmt]
+      @connection.get '/v1/gifs/trending', @api_key
     end
 
     def random
-      @connection.get '/v1/gifs/random', { :api_key => 'dc6zaTOxFJmzC' }
+      #[tag,rating,fmt]
+      @connection.get '/v1/gifs/random', @api_key
+    end
+
+    def translate(query)
+      #[s,rating,fmt]
+      @connection.get "/v1/gifs/translate?#{get_paras(query)}", @api_key 
+
+    end
+
+    private
+
+    def get_paras(paras)
+
+      paras.inject(""){|a,(k,v)| a << "&#{k}=#{v}"}
+
     end
 
   end
